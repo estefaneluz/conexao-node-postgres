5b4afa91008a382dafb475b04a738fd91b084d49
estefaneluz <estefanecortes@gmail.com>
2021-07-26 21:40:21 -0300

    funcao editar autor criada

--- classe-01/controllers/autores.js
+++ classe-01/controllers/autores.js
@@ -49,7 +49,23 @@ const cadastrarAutor = async (req, res) => {
 }
 
 const editarAutor = async (req, res) => {
+    const {id} = req.params;
+    const {nome, idade} = req.body;
 
+    try {
+        const autor = await conexao.query("SELECT * FROM autores WHERE id = $1", [id]);
+        
+        if(!autor.rowCount) return res.status(404).json("O autor informado não foi encontrado.");
+        if(!nome) return res.status(400).json("O campo nome precisa ser informado");
+
+        const autorEditado = await conexao.query("UPDATE autores SET nome = $1, idade = $2 WHERE id = $3", [nome, idade, id]);
+
+        if(!autorEditado.rowCount) return res.status(400).json("Não foi possível atualizar o autor.");
+
+        return res.status(200).json("Autor atualizado com sucesso!");
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 const deletarAutor = async (req, res) => {
