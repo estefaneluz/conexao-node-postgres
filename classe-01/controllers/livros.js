b92e27333d1b68bc578beaf9a78491eb3ba272f3
estefaneluz <estefanecortes@gmail.com>
2021-07-26 22:05:06 -0300

    funcao listarLivros criada

--- classe-01/controllers/livros.js
+++ classe-01/controllers/livros.js
@@ -1,5 +1,12 @@
-const listarLivros = async (req, res) => {
+const conexao = require('../connection');
 
+const listarLivros = async (req, res) => {
+    try {
+        const { rows: livros } = await conexao.query("SELECT * FROM livros");
+        return res.status(200).json(livros);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 const obterLivro = async (req, res) => {
