88cf35dd125a34c2b7df0dd4bad312f806fbf0ad
estefaneluz <estefanecortes@gmail.com>
2021-07-26 22:15:06 -0300

    funcao cadastrarLivro criada

--- classe-01/controllers/livros.js
+++ classe-01/controllers/livros.js
@@ -21,7 +21,19 @@ const obterLivro = async (req, res) => {
 }
 
 const cadastrarLivro = async (req, res) => {
-
+    const {autor_id, nome, editora, genero, data_publicacao } = req.body;
+    if(!autor_id) return res.status(400).json("O id do autor precisa ser informado.");
+    if(!nome) return res.status(400).json("O nome do livro precisa ser informado.");
+    if(!genero) return res.status(400).json("O gênero do livro precisa ser informado.");
+    try {
+        const livro = await conexao.query(
+            "INSERT INTO livros (autor_id, nome, editora, genero, data_publicacao) VALUES ($1, $2, $3, $4, $5)",
+            [autor_id, nome, editora, genero, data_publicacao]);
+        if(!livro.rowCount) return res.status(400).json("Não foi possível cadastrar o livro.");
+        return res.status(200).json("Livro cadastrado com sucesso!");
+    } catch(error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 const editarLivro = async (req, res) => {
