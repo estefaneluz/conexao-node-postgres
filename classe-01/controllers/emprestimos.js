a576df444d986e4c69b58f6a23bd2553223069a9
estefaneluz <estefanecortes@gmail.com>
2021-07-28 17:46:17 -0300

    funcao cadastrarEmprestimo criada

--- classe-01/controllers/emprestimos.js
+++ classe-01/controllers/emprestimos.js
@@ -21,11 +21,15 @@ const obterEmprestimo = async (req, res) => {
 }
 
 const cadastrarEmprestimo = async (req, res) => {
-    const { autor_id, livro_id } = req.body;
-    if(!autor_id) return res.status(400).json("Precisa ser informado o id do autor.");
+    const { usuario_id, livro_id } = req.body;
+    if(!usuario_id) return res.status(400).json("Precisa ser informado o id do usuário.");
     if(!livro_id) return res.status(400).json("Precisa ser informado o id do livro.");
     try {
-
+        const emprestimo = await conexao.query(
+            `INSERT INTO emprestimos (usuario_id, livro_id) VALUES ($1, $2)`, 
+            [usuario_id, livro_id]);
+        if(!emprestimo.rowCount) return res.status(400).json("Não foi possível cadastrar o emprestimo.");
+        return res.status(200).json("Emprestimo cadastrado com sucesso!");
     } catch (error) {
         return res.status(400).json(error.message);
     }
