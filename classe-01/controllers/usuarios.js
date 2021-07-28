d6a30d0b95b68c9a5a0e0e93a51360b21a9d3f27
estefaneluz <estefanecortes@gmail.com>
2021-07-28 16:54:38 -0300

    listar usuarios criado

--- classe-01/controllers/usuarios.js
+++ classe-01/controllers/usuarios.js
@@ -1,7 +1,12 @@
 const conexao = require('../connection');
 
 const listarUsuarios = (req, res) => {
-
+    try {
+        const { rows: usuarios } = await conexao.query("SELECT * FROM usuarios");
+        return res.status(200).json(usuarios);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 }
 
 const obterUsuario = (req, res) => {
