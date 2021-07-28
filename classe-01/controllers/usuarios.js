583d7adc3f8c4b583678207e411f9d0e582ad872
estefaneluz <estefanecortes@gmail.com>
2021-07-28 17:25:04 -0300

    funcao editarUsuario criada

--- classe-01/controllers/usuarios.js
+++ classe-01/controllers/usuarios.js
@@ -48,7 +48,11 @@ const cadastrarUsuario = async (req, res) => {
     if(!email.includes('@') || !email.includes(".")) return res.status(400).json("Insira um e-mail válido.");
 
     try {
-        
+        const usuario = await conexao.query("SELECT * FROM usuarios WHERE id = $1", [id]);
+        if(!usuario.rowCount) return res.status(404).json("Usuário não encontrado.");
+        const usuarioAtualizado = await conexao.query("UPDATE usuarios SET nome = $1, idade = $2, email = $3, telefone = $4, cpf = $5 WHERE id = $6", [nome, idade, email, telefone, cpf, id]);
+        if(!usuarioAtualizado.rowCount) return res.status(400).json("Não foi possível atualizar o usuário.");
+        return res.status(200).json("Usuário atualizado!");
     } catch (error) {
         return res.status(400).json(error.message);
     }
