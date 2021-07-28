f6705bb3c08a2829bc79aa7b6adf05d9da64cae7
estefaneluz <estefanecortes@gmail.com>
2021-07-28 16:59:35 -0300

    funcao obterUsuario criada

--- classe-01/controllers/usuarios.js
+++ classe-01/controllers/usuarios.js
@@ -1,6 +1,6 @@
 const conexao = require('../connection');
 
-const listarUsuarios = (req, res) => {
+const listarUsuarios = async (req, res) => {
     try {
         const { rows: usuarios } = await conexao.query("SELECT * FROM usuarios");
         return res.status(200).json(usuarios);
@@ -9,19 +9,26 @@ const listarUsuarios = (req, res) => {
     }
 }
 
-const obterUsuario = (req, res) => {
+const obterUsuario =  async (req, res) => {
+    const { id } = req.params; 
+    try {
+        const usuario = await conexao.query("SELECT * FROM usuarios WHERE id = $1", [id]);
+        if(!usuario.rowCount) return res.status(404).json("Não foi possível encontrar o usuário.");
+        return res.status(200).json(usuario.rows[0]);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const cadastrarUsuario = async (req, res) => {
 
 }
 
-const cadastrarUsuario = (req, res) => {
-
-}
-
- const editarUsuario = (req, res) => {
+ const editarUsuario = async (req, res) => {
 
  }
 
- const deletarUsuario = (req, res) => {
+ const deletarUsuario = async (req, res) => {
 
  }
 
