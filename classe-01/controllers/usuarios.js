241b9890852ecc433ee4786eed4725da1adf5fd1
estefaneluz <estefanecortes@gmail.com>
2021-07-28 16:47:39 -0300

    rotas de usuario criada

---
+++ classe-01/controllers/usuarios.js
@@ -0,0 +1,29 @@
+const conexao = require('../connection');
+
+const listarUsuarios = (req, res) => {
+
+}
+
+const obterUsuario = (req, res) => {
+
+}
+
+const cadastrarUsuario = (req, res) => {
+
+}
+
+ const editarUsuario = (req, res) => {
+
+ }
+
+ const deletarUsuario = (req, res) => {
+
+ }
+
+module.exports = {
+    listarUsuarios,
+    obterUsuario,
+    cadastrarUsuario, 
+    editarUsuario,
+    deletarUsuario
+}
--- classe-01/routes.js
+++ classe-01/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const autores = require('./controllers/autores');
 const livros = require('./controllers/livros');
+const usuarios = require('./controllers/usuarios');
 
 const routes = express();
 
@@ -17,5 +18,12 @@ routes.get('/livros/:id', livros.obterLivro);
 routes.post('/livros', livros.cadastrarLivro); 
 routes.put('/livros/:id', livros.editarLivro);
 routes.delete('/livros/:id', livros.deletarLivro);
+
+//usuarios
+routes.get('/usuarios', usuarios.listarUsuarios);
+routes.get('/usuarios/:id', usuarios.obterUsuario);
+routes.get('/usuarios', usuarios.cadastrarUsuario);
+routes.get('/usuarios/:id', usuarios.editarUsuario);
+routes.get('/usuarios/:id', usuarios.deletarUsuario);
 
 module.exports = routes;
